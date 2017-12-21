de5361f1cac28635e16797f2e6f47b625de7f0b1
Jake Zarobsky <jzarobsky@Insuresoft.com>
2017-12-21 12:54:17 -0600

    Update to add some additional testing methods

---
+++ DocAttributesTests/Targets/MethodTargetTest.cs
@@ -0,0 +1,56 @@
+﻿using DocAttributes.Targets;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocAttributesTests.Targets
+{
+    [TestFixture]
+    public class MethodTargetTest
+    {
+        private TypeTarget instance;
+        private MethodTarget methodTarget;
+
+        [SetUp]
+        public void Setup()
+        {
+            instance = new TypeTarget(typeof(TypeTargetTesterClass));
+            methodTarget = instance.Methods.Where(x => x.Name == "SomeGenericMethod").FirstOrDefault();
+        }
+
+        [Test]
+        public void classShouldHaveMultipleMethods()
+        {
+            Assert.GreaterOrEqual(instance.Methods.Count, 1);
+        }
+
+        [Test]
+        public void methodTarget_shouldNotBeNull()
+        {
+            Assert.NotNull(methodTarget);
+        }
+
+        [Test]
+        public void methodTarget_summaryShouldHaveCorrectValue()
+        {
+            var correctValue = TypeTargetTesterClass.METHOD_TARGET_SUMMARY;
+            Assert.AreEqual(correctValue, methodTarget.Summary);
+        }
+
+        [Test]
+        public void methodTarget_returnsShouldHaveCorrectValue()
+        {
+            var correctValue = TypeTargetTesterClass.METHOD_TARGET_RETURNS;
+            Assert.AreEqual(correctValue, methodTarget.ReturnDescription);
+        }
+
+        [Test]
+        public void methodTarget_shouldCorrectlyDetermineReturnType()
+        {
+            Assert.AreEqual(typeof(string), methodTarget.ReturnType);
+        }
+    }
+}
--- DocAttributesTests/Targets/TargetTesterClass.cs
+++ DocAttributesTests/Targets/TargetTesterClass.cs
@@ -10,6 +10,8 @@ namespace DocAttributesTests.Targets
     {
         public const string TYPE_TARGET_SUMMARY = "Type target summary";
         public const string TYPE_TARGET_VERSION = "Type target version";
+        public const string METHOD_TARGET_SUMMARY = "Method target summary";
+        public const string METHOD_TARGET_RETURNS = "Method target returns";
         
         public int Add(int a, int b)
         {
@@ -18,5 +20,14 @@ namespace DocAttributesTests.Targets
             
         public int SomeProperty { get; set; }
         public int SomeSecondField;
+
+        [Summary(METHOD_TARGET_SUMMARY)]
+        [Returns(METHOD_TARGET_RETURNS)]
+        [AvailableSince("1.0")]
+        public string SomeGenericMethod<[Summary("The first type")] T1,
+            [Summary("The second type parameter")] T2>([Summary("The first parameter")] T1 obj1, [Summary("The second parameter")] T2 obj2)
+        {
+            return string.Empty;
+        }
     }
 }
